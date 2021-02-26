bce3e28f50b1ad8836bbbcaf35689dda2a72027e
Fabián Santander <hdmsantander@gmail.com>
2021-02-26 09:49:55 -0600

    Set initial data parsing for archinaut results

--- index.js
+++ index.js
@@ -2,17 +2,24 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const fs = require('fs');
 const csvToJson = require('convert-csv-to-json');
+const aws = require('aws-sdk/clients/lambda');
+
 
 try {
 
   // Our github context
   const payload = github.context.payload;
 
-  console.log("Context: " + JSON.stringify(github.context));
+  const input = core.getInput('results');
 
-  // Get commit id and timestamp
   const commitId = payload.commits[0].id
   const date = payload.commits[0].timestamp;
+  const repoFullName = payload.repository.full_name;
+
+  const archinautResults = csvToJson.getJsonFromCsv(input);
+
+  console.log(`Commit id: ${commitId} date: ${date} repository name: ${repoFullName}`);
+  console.log("Results: " + JSON.stringify(archinautResults));
 
 } catch (error) {
   core.setFailed(error.message);
