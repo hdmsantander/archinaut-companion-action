d301fe42dd36b2cad4210a5412a881bb51b3c4f1
Fabián Santander <hdmsantander@gmail.com>
2021-07-05 20:54:25 -0500

    Change usage to reflect new logic

--- index.js
+++ index.js
@@ -3,13 +3,7 @@ const github = require('@actions/github');
 const csv = require('csvtojson');
 const fs = require('fs');
 
-import Lambda from 'aws-sdk/clients/lambda';
-import AWS from 'aws-sdk/global';
-
-const apiVersion = '2015-03-31';
-
 try {
-  // Our github context
   const payload = github.context.payload;
 
   const input = core.getInput('results');
@@ -17,17 +11,6 @@ try {
   const commitId = payload.commits[0].id;
   const date = payload.commits[0].timestamp;
   const repoFullName = payload.repository.full_name;
-
-  const awsCredentials = new AWS.Credentials();
-  awsCredentials.accessKeyId = core.getInput('AWS_ACCESS_KEY_ID');
-  awsCredentials.secretAccessKey = core.getInput('AWS_SECRET_ACCESS_KEY');
-
-  const awsConfig = new AWS.Config({
-    credentials: awsCredentials,
-    region: core.getInput('REGION'),
-  });
-
-  const lambda = new Lambda({ apiVersion, region: core.getInput('REGION') });
 
   csv()
     .fromFile(input)
@@ -39,14 +22,7 @@ try {
       output.id = repoFullName.split('/').join('-');
       output.files = parseResult;
 
-      console.log('Results: ' + JSON.stringify(output));
-
-      lambda.invoke({
-        FunctionName: 'putDataAnalysis',
-        Payload: JSON.stringify(output),
-        InvocationType: 'RequestResponse',
-        LogType: 'None',
-      });
+      fs.writeFile('archinaut.json', JSON.stringify(output), 'utf8');
     });
 } catch (error) {
   core.setFailed(error.message);
