f60d7a4620465776bc05063e6426550fb18b5ec5
Fabián Santander <hdmsantander@gmail.com>
2021-07-05 20:43:42 -0500

    Add fs package

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const csv = require("csvtojson"); 
+const csv = require('csvtojson');
+const fs = require('fs');
 
 import Lambda from 'aws-sdk/clients/lambda';
-import AWS from 'aws-sdk/global'
+import AWS from 'aws-sdk/global';
 
 const apiVersion = '2015-03-31';
 
 try {
-
   // Our github context
   const payload = github.context.payload;
 
   const input = core.getInput('results');
 
-  const commitId = payload.commits[0].id
+  const commitId = payload.commits[0].id;
   const date = payload.commits[0].timestamp;
   const repoFullName = payload.repository.full_name;
 
@@ -23,26 +23,31 @@ try {
   awsCredentials.secretAccessKey = core.getInput('AWS_SECRET_ACCESS_KEY');
 
   const awsConfig = new AWS.Config({
-    credentials: awsCredentials, region: core.getInput('REGION')
-  })
+    credentials: awsCredentials,
+    region: core.getInput('REGION'),
+  });
 
-  const lambda = new Lambda({apiVersion, region: core.getInput('REGION')});
+  const lambda = new Lambda({ apiVersion, region: core.getInput('REGION') });
 
-  csv().fromFile(input).then((parseResult) => {
+  csv()
+    .fromFile(input)
+    .then((parseResult) => {
+      let output = {};
 
-    let output = {};
+      output.idCommit = commitId;
+      output.date = date;
+      output.id = repoFullName.split('/').join('-');
+      output.files = parseResult;
 
-    output.idCommit = commitId;
-    output.date = date;
-    output.id = repoFullName.split("/").join("-");
-    output.files = parseResult;
+      console.log('Results: ' + JSON.stringify(output));
 
-    console.log("Results: " + JSON.stringify(output));
-
-    lambda.invoke({FunctionName:'putDataAnalysis', Payload: JSON.stringify(output), InvocationType: 'RequestResponse', LogType: 'None'});
-
-  })
-
+      lambda.invoke({
+        FunctionName: 'putDataAnalysis',
+        Payload: JSON.stringify(output),
+        InvocationType: 'RequestResponse',
+        LogType: 'None',
+      });
+    });
 } catch (error) {
   core.setFailed(error.message);
 }
